17926a4c02db8a8248c5370126bf0e41eb5a0eb2
jccruz-qltp <158495863+jccruz-qltp@users.noreply.github.com>
2026-06-18 14:51:30 -0600

    Update test_csv.py

--- python/test_csv.py
+++ python/test_csv.py
@@ -1,19 +1,27 @@
-import csv
+import sys, csv
 from pydantic import  ValidationError
 from models.csv_user import User
 
+def validar_csv(fileName: str) -> none:
+    # 2. Read and validate rows
+    valid_users = []
 
-# 2. Read and validate rows
-valid_users = []
-
-with open("users.csv", mode="r", encoding="utf-8") as f:
-    reader = csv.DictReader(f)
+    with open(fileName, mode="r", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
     
-    for row_number, row in enumerate(reader, start=2): # Header is line 1
-        try:
-            # model_validate parses the dictionary and enforces types
-            user = User.model_validate(row)
-            valid_users.append(user)
-        except ValidationError as e:
-            print(f"Row {row_number} failed validation:")
-            print(e.errors())
+        for row_number, row in enumerate(reader, start=2): # Header is line 1
+            try:
+                # model_validate parses the dictionary and enforces types
+                user = User.model_validate(row)
+                valid_users.append(user)
+            except ValidationError as e:
+                print(f"Row {row_number} failed validation:")
+                print(e.errors())
+
+
+if __name__ == "__main__":
+    argc = len(sys.argv) 
+    filename = "users.csv"
+    if argc > 1
+        filename = sys.argv[1]
+    validar_csv(filename)  
