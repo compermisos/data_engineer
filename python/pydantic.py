9619cb702acc06af10bc1bbd2c8283094fb622b0
jccruz-qltp <158495863+jccruz-qltp@users.noreply.github.com>
2026-06-18 13:14:12 -0600

    Create pydantic.py

---
+++ python/pydantic.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from pydantic import BaseModel, PositiveInt
+
+
+class User(BaseModel):
+  id: int  
+  name: str = 'John Doe'  
+  signup_ts: datetime | None  
+  tastes: dict[str, PositiveInt]  
+
+
+test_data = {
+  'id': 123,
+  'signup_ts': '2019-06-01 12:22',  
+  'tastes': {
+      'wine': 9,
+      b'cheese': 7,  
+      'cabbage': '1',  
+  },
+}
+
+if __name__ == "__main__":
+    user = User(**test_data)  
+    print(user.id)  
+    print(user.model_dump())  
+"""
+{
+  'id': 123,
+  'name': 'John Doe',
+  'signup_ts': datetime.datetime(2019, 6, 1, 12, 22),
+  'tastes': {'wine': 9, 'cheese': 7, 'cabbage': 1},
+}
+"""
