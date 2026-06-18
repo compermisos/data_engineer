a5de9225c586fe7d6df3839ac28e814847163fd1
jccruz-qltp <158495863+jccruz-qltp@users.noreply.github.com>
2026-06-18 14:41:42 -0600

    Create test_csv.py

---
+++ python/test_csv.py
@@ -0,0 +1,19 @@
+import csv
+from pydantic import  ValidationError
+import models/user_csv as csv_user
+
+
+# 2. Read and validate rows
+valid_users = []
+
+with open("users.csv", mode="r", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+    
+    for row_number, row in enumerate(reader, start=2): # Header is line 1
+        try:
+            # model_validate parses the dictionary and enforces types
+            user = csv_user.User.model_validate(row)
+            valid_users.append(user)
+        except ValidationError as e:
+            print(f"Row {row_number} failed validation:")
+            print(e.errors())
